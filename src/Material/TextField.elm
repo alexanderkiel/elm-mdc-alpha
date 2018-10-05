83624cc601b33427f82b6980b2cd695841da54b6
ari <info@iba-cg.de>
2018-10-05 13:27:25 +0200

    Added to TextField: SimpleModel, withLeading/TrailingIcon

--- src/Material/TextField.elm
+++ src/Material/TextField.elm
@@ -1,6 +1,6 @@
 module Material.TextField exposing
-    ( Model
-    , SimpleModel
+    ( SimpleModel
+    , Model
     , init
     , Msg
     , update
@@ -8,6 +8,8 @@ module Material.TextField exposing
     , fullWidth
     , outlined
     , label
+    , withLeadingIcon
+    , withTrailingIcon
     )
 
 {-| Text fields let users enter and edit text.
@@ -47,6 +49,8 @@ In your Sass file import:
 @docs fullWidth
 @docs outlined
 @docs label
+@docs withLeadingIcon
+@docs withTrailingIcon
 
 
 # Reference
@@ -58,11 +62,12 @@ In your Sass file import:
 
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Material.Icon as Icon
 import Material.Internal.Options as Options exposing (class, styled, when)
+import Parser as SimpleParser exposing (Problem)
 import Parser.Advanced as Parser exposing (Parser)
 
 
-import Parser as SimpleParser exposing (Problem)
 
 ---- MODEL --------------------------------------------------------------------
 
@@ -76,7 +81,10 @@ type alias Model context problem value =
     , input : Maybe String
     }
 
-type alias SimpleModel value = Model Never SimpleParser.Problem value
+
+type alias SimpleModel value =
+    Model Never SimpleParser.Problem value
+
 
 {-| -}
 init :
@@ -226,6 +234,10 @@ view requiredConfig model properties _ =
 
         isDirty =
             finalValue /= ""
+
+        iconAllowed =
+            -- only in default or outline mode
+            not config.textarea
     in
     Options.apply summary
         Html.div
@@ -237,9 +249,20 @@ view requiredConfig model properties _ =
         , class "mdc-text-field--invalid" |> when isInvalid
         , class "mdc-text-field--textarea" |> when config.textarea
         , class "mdc-text-field--outlined" |> when config.outlined
+        , class "mdc-text-field--with-leading-icon" |> when (config.leadingIcon /= Nothing)
+        , class "mdc-text-field--with-trailing-icon" |> when (config.trailingIcon /= Nothing)
         ]
         []
-        [ styled
+        [ -- icon
+          if Nothing == config.leadingIcon || not iconAllowed then
+            Html.text ""
+
+          else
+            Icon.view
+                [ Options.class "mdc-text-field__icon" ]
+                (config.leadingIcon |> Maybe.withDefault "unknown")
+        , -- input or textArea
+          styled
             (if config.textarea then
                 Html.textarea
 
@@ -256,7 +279,9 @@ view requiredConfig model properties _ =
             , Options.onInput <| (config.lift << Input)
             ]
             []
-        , if not config.fullWidth then
+        , -- label
+          -- no label in fullWidth
+          if not config.fullWidth then
             styled
                 Html.label
                 [ class "mdc-floating-label"
@@ -271,6 +296,20 @@ view requiredConfig model properties _ =
                     Nothing ->
                         []
                 )
+
+          else
+            Html.text ""
+        , -- icon
+          if Nothing == config.trailingIcon || not iconAllowed then
+            Html.text ""
+
+          else
+            Icon.view
+                [ Options.class "mdc-text-field__icon" ]
+                (config.trailingIcon |> Maybe.withDefault "unknown")
+        , if config.outlined then
+            -- TODO how should we add svg for outlined and label above problem
+            styled Html.div [ class "mdc-notched-outline__idle" ] []
 
           else
             Html.text ""
@@ -302,3 +341,15 @@ outlined =
 label : String -> Property msg value
 label str =
     Options.updateConfig (\config -> { config | labelText = Just str })
+
+
+{-| -}
+withLeadingIcon : String -> Property msg value
+withLeadingIcon iconName =
+    Options.updateConfig (\config -> { config | leadingIcon = Just iconName })
+
+
+{-| -}
+withTrailingIcon : String -> Property msg value
+withTrailingIcon iconName =
+    Options.updateConfig (\config -> { config | trailingIcon = Just iconName })
